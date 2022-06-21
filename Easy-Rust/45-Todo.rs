f54fb060d40ffb33982b295cdc93d70a78cee46d
TriCao <106253830+Dev7Boy7@users.noreply.github.com>
2022-06-21 15:03:03 +0700

    Create 45-Todo.rs

---
+++ Easy-Rust/45-Todo.rs
@@ -0,0 +1,22 @@
+enum HocTap {
+    Hocbai,
+    Ghichu,
+    
+}
+
+fn lo_trinh() -> String {
+   todo!()
+} 
+
+fn muc_tieu() -> String {
+    todo!()
+}
+fn main () {
+        let _aaa = HocTap::Hocbai;
+        let _bbb = HocTap::Ghichu;
+
+}
+
+// todo!() // todo means "I will do it later, please be quiet"
+
+//todo!() is actually the same as another macro: unimplemented!(). Programmers were using unimplemented!() a lot but it was long to type, so they created todo!() which is shorter
