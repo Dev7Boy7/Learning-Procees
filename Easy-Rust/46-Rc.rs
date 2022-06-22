f04373fd98a048e0af86f2fdf4fa90679ea6a857
TriCao <106253830+Dev7Boy7@users.noreply.github.com>
2022-06-22 18:24:43 +0700

    Update 46-Rc.rs

--- Easy-Rust/46-Rc.rs
+++ Easy-Rust/46-Rc.rs
@@ -5,34 +5,35 @@
 
 //Rc writes down who has ownership, and how many 
 
-// #[derive(Debug)]
-// struct quan_huyen {
-//     ten_quan_huyen : String,
-//     dan_so : u32,
-//     lich_su_quan_huyen : String,
-// }
-// #[derive(Debug)]
-// struct dulieu_quanhuyen {
-//     name: Vec<String>,
-//     lich_su : Vec<String>,
-// }
+#[derive(Debug)]
+struct quan_huyen {
+    ten_quan_huyen : String,
+    dan_so : u32,
+    lich_su_quan_huyen : String,
+}
+#[derive(Debug)]
+struct dulieu_quanhuyen {
+    name: Vec<String>,
+    lich_su : Vec<String>,
+}
 
-// fn main() {
-//     let quan_binh_tan = quan_huyen {
-//         ten_quan_huyen: "Binh Tan".to_string(),
-//         dan_so : 1_000_000,
-//         lich_su_quan_huyen: "Tach ra tu binh chanh".to_string(),
-//     };
+fn main() {
+    let quan_binh_tan = quan_huyen {
+        ten_quan_huyen: "Binh Tan".to_string(),
+        dan_so : 1_000_000,
+        lich_su_quan_huyen: "Tach ra tu binh chanh".to_string(),
+    };
 
-//     let binh_tan = dulieu_quanhuyen {
-//         name : vec![quan_binh_tan.ten_quan_huyen], // dữ liệu đã được chuyển đến đây
-//         lich_su : vec![quan_binh_tan.lich_su_quan_huyen], // dữ liệu đã được chuyển đến đây
-//     };
+    let binh_tan = dulieu_quanhuyen {
+        name : vec![quan_binh_tan.ten_quan_huyen], // dữ liệu đã được chuyển đến đây
+        lich_su : vec![quan_binh_tan.lich_su_quan_huyen], // dữ liệu đã được chuyển đến đây
+    };
 
-//     // println!("{:?}", quan_binh_tan);
+    // println!("{:?}", quan_binh_tan);
 
-//     println!("{:?}", binh_tan);
-// 
+    println!("{:?}", binh_tan);
+    
+//--------------------------------------------------------------------------------------------- 
 
 use std::rc::Rc;
 
