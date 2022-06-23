dc7c6c8c86012af29514e74b58d43bc9a6d7404a
TriCao <106253830+Dev7Boy7@users.noreply.github.com>
2022-06-23 16:12:38 +0700

    Create 005-Semi-Structured-Logs.rs

---
+++ Exercism/005-Semi-Structured-Logs.rs
@@ -0,0 +1,25 @@
+#![allow(unused)]
+/// various log levels
+#[derive(Clone, PartialEq, Debug)]
+pub enum LogLevel {
+    Info,
+    Warning,
+    Error,
+}
+/// primary function for emitting logs
+pub fn log(level: LogLevel, message: &str) -> String {
+    match level {
+        LogLevel::Info => format!("[INFO]: {}", message),
+        LogLevel::Warning => format!("[WARNING]: {}", message),
+        LogLevel:: Error => format!("[ERROR]: {}", message),
+    }
+}
+pub fn info(message: &str) -> String {
+    format!("[INFO]: {}", message)
+}
+pub fn warn(message: &str) -> String {
+    format!("[WARNING]: {}", message)
+}
+pub fn error(message: &str) -> String {
+    format!("[ERROR]: {}", message)
+}
