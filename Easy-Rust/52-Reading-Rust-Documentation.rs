a1940f50dd306506aa7c36181cf5998cdc40f5a6
TriCao <106253830+Dev7Boy7@users.noreply.github.com>
2022-06-24 15:45:04 +0700

    Update 52-Reading-Rust-Documentation.rs

--- Easy-Rust/52-Reading-Rust-Documentation.rs
+++ Easy-Rust/52-Reading-Rust-Documentation.rs
@@ -4,15 +4,15 @@
 // assert_eq!
 // Doing testing - program will panic if they not equal 
 
-// fn testing(input: u8) {
-//     assert_eq!(input%2, 0);
-//     println!("The number is not odd. It is {}", input)
-// }
-// fn main() {
-//     testing(60);
+fn testing(input: u8) {
+    assert_eq!(input%2, 0);
+    println!("The number is not odd. It is {}", input)
+}
+fn main() {
+    testing(60);
 
-//     testing(51);
-// }
+    testing(51);
+}
 
 // Searching 
 
