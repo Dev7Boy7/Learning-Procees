f8d7f1417c9b2f73d2789d47aadcdd23c9927d06
TriCao <106253830+Dev7Boy7@users.noreply.github.com>
2022-06-24 15:44:45 +0700

    Create 52-Reading-Rust-Documentation.rs

---
+++ Easy-Rust/52-Reading-Rust-Documentation.rs
@@ -0,0 +1,19 @@
+
+// Reading Rust Documentation
+
+// assert_eq!
+// Doing testing - program will panic if they not equal 
+
+// fn testing(input: u8) {
+//     assert_eq!(input%2, 0);
+//     println!("The number is not odd. It is {}", input)
+// }
+// fn main() {
+//     testing(60);
+
+//     testing(51);
+// }
+
+// Searching 
+
+// Information on traits
